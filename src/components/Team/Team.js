fb1bb87db8fe3a3efc581b4073d2ee7bfe94a76d
felipebc2 <felipeb.castro02@gmail.com>
2025-05-20 15:43:44 -0300

    TeamColors

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,13 @@
 import './Team.css'
 
 export const Team = (props) => {
+
+    const secColor = { backgroundColor: props.corSecundaria }
+    const h3Color = { borderColor: props.corPrimaria }
+
     return (
-        <section className='team'>
-            <h3>{props.nome}</h3>
+        <section className='team' style={secColor}>
+            <h3 style={h3Color}>{props.nome}</h3>
         </section>
     )
 }
